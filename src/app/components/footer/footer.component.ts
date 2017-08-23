e385e322c63deda84240c30327e0c987b314e0cc
lucaxs09 <lucahernan66@gmail.com>
2017-08-23 04:00:09 -0300

    Header, Body, Footer listos.

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styles: []
+})
+export class FooterComponent {
+  public anio:number = new Date().getFullYear();
+
+
+
+}
